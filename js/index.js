b585b74d7fbf8229e64dd828e805489c0baecae1
irdepla <rixsiboyevislombek@gmail.com>
2024-10-14 20:35:41 +0500

    add: join section

--- js/index.js
+++ js/index.js
@@ -43,29 +43,29 @@ closeBtn.addEventListener("click", () => {
 //   };
 // }
 
-// function createCounter() {
-//   let count = 0;  
+function createCounter() {
+  let count = 0;  
 
-//   return {
-//       increment: function() {
-//           count++;  
-//           return count;  
-//       },
-//       decrement: function() {
-//           count--;  
-//           return count; 
-//       }
-//   };
-// }
-
-
+  return {
+      increment: function() {
+          count++;  
+          return count;  
+      },
+      decrement: function() {
+          count--;  
+          return count; 
+      }
+  };
+}
 
 
 
 
-// const counter = createCounter();
 
-// console.log(counter.increment()); 
-// console.log(counter.increment()); 
-// console.log(counter.decrement()); 
-// console.log(counter.decrement()); 
+
+const counter = createCounter();
+
+console.log(counter.increment()); 
+console.log(counter.increment()); 
+console.log(counter.decrement()); 
+console.log(counter.decrement()); 
